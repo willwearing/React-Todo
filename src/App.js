248d147a3b853e72ae5aa8919eb3f6a761c110ec
willwearing <willwearing@gmail.com>
2020-09-28 15:28:28 -0600

    styled components - ready for hosting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import "./components/Todo.css";
 
 //making basic to do list
 const toDo = [
